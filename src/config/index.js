9b23c22a4ca9d6cb78fcfcdf2969d305df12b17e
tunogya <20251942+tunogya@users.noreply.github.com>
2024-03-10 03:25:48 +0800

    update

--- src/config/index.js
+++ src/config/index.js
@@ -6,5 +6,11 @@ module.exports = {
   auth0: {
     audience: process.env.AUTH0_AUDIENCE,
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-  }
+  },
+  mongodb: {
+    uri: process.env.MONGODB_URI
+  },
+  stripe: {
+    secret: process.env.STRIPE_SECRET_KEY
+  },
 }
---
+++ src/config/mongodb.js
@@ -0,0 +1,13 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const config = require('./index');
+
+const client = new MongoClient(config.mongodb.uri,  {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    }
+);
+
+module.exports = client
