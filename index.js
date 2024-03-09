3250fe8ffd63502aec04ef9aff10c1d2b36e9247
tunogya <20251942+tunogya@users.noreply.github.com>
2024-03-10 02:24:54 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const serverless = require("serverless-http");
 const express = require("express");
+const { auth } = require('express-oauth2-jwt-bearer');
 const app = express();
+
+const jwtCheck = auth({
+  audience: 'https://api.abandon.ai',
+  issuerBaseURL: 'https://abandon.jp.auth0.com/',
+  tokenSigningAlg: 'RS256'
+});
+
+app.use(jwtCheck);
 
 app.get("/", (req, res, next) => {
   return res.status(200).json({
